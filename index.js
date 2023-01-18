e0b90768008f4cb45169c998214582bbdb697478
witehound <netvid00@gmail.com>
2023-01-18 12:02:29 +0100

    added cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const { createMongooseConnection } = require("./utils/mongoose.js");
 const pinRouter = require("./routes/pin.js");
 const userRouter = require("./routes/user.js");
+const cors = require("cors");
 
 createMongooseConnection();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/pin", pinRouter);
 app.use("/api/user", userRouter);
